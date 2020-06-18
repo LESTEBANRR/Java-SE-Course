1d48f02f81767f240d14f52c925f2578060abc3d
LESTEBANRR <l_esteban_1998@hotmail.com>
2020-06-18 16:35:00 -0500

    Tipos de datos Enteros

--- JavaSE/HolaMundo/src/com/LERR/holamundo/HolaMundo.java
+++ JavaSE/HolaMundo/src/com/LERR/holamundo/HolaMundo.java
@@ -4,6 +4,11 @@ public class HolaMundo {
 
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
+		// Tipos de Datos Enteros
+		byte edad = 127;
+		short year = -32768;
+		int id_user = 1001;
+		long id_twiteer=11234567891234567L;
+		
 	}
-
 }
