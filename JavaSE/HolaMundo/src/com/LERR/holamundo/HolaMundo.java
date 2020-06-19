1fc505687180b6f74bc971e7538b884651a70a0e
LESTEBANRR <l_esteban_1998@hotmail.com>
2020-06-19 12:06:53 -0500

    For anidado

--- JavaSE/HolaMundo/src/com/LERR/holamundo/HolaMundo.java
+++ JavaSE/HolaMundo/src/com/LERR/holamundo/HolaMundo.java
@@ -177,7 +177,18 @@ public class HolaMundo {
 		for (int i : numeros) {
 			System.out.println(i);
 		}
-		System.out.println("Continuamos con el flujo");
-		
+		System.out.println("Continuamos con el flujo");					 
+		 
+		//FORAECH ANIDADO
+		        //Definimos un array de 3 filas x 5 columnas
+		        int array[][]={{1,2,3,4,5}, {6,7,8,9,10}, {11,12,13,14,15}};
+		 
+		        //Recorremos el array multidimensional
+		        for (int[] arrayInterno : array){
+		            for(int numero: arrayInterno){
+		                System.out.println(numero);
+		            }
+		        }
+	
 	}
 }
