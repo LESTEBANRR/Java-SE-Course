8ca59ff34b2b5d8a8373c424947ca287db0ed1e1
LESTEBANRR <l_esteban_1998@hotmail.com>
2020-06-19 11:18:37 -0500

    If y Switch

--- JavaSE/HolaMundo/src/com/LERR/holamundo/HolaMundo.java
+++ JavaSE/HolaMundo/src/com/LERR/holamundo/HolaMundo.java
@@ -86,20 +86,77 @@ public class HolaMundo {
 		//System.out.println(q!=p);
 		
 		//Operadores de Relación
+		/*
 		System.out.println("q < p ->"+(q < p));
 		System.out.println("q > p ->"+(q > p));
 		
 		System.out.println("q <= p ->"+(q <= p));
 		System.out.println("q >= p ->"+(q >= p));
-		
+		*/
 		//Operadores Lógicos
 		boolean n=false;
 		boolean m=true;
 		
+		/*
 		System.out.println("n && m ->"+(n&&m));
 		System.out.println("n || m ->"+(n||m));
 		
 		System.out.println("!n ->"+!n);
 		System.out.println("!m ->"+!m);
+		*/
+		
+		//--------------Control de Flujo-----------------------------
+		//-----IF ELSE--------------
+		if (q>p) {
+		System.out.println("q es mayor que p");
+		}else if (q==p) {
+			System.out.println("q y p son iguales");
+		}else {
+			System.out.println("q es menor que p");
+		}
+		//-----Switch--------------
+		int mes =8;
+		switch (mes) {
+			case 1:
+				System.out.println("Enero");
+				break;
+			case 2:
+				System.out.println("Febrero");
+				break;
+			case 3:
+				System.out.println("Marzo");
+				break;
+			case 4:
+				System.out.println("Abril");
+				break;
+			case 5:
+				System.out.println("Mayo");
+				break;
+			case 6:
+				System.out.println("Junio");
+				break;
+			case 7:
+				System.out.println("Julio");
+				break;
+			case 8:
+				System.out.println("Agosto");
+				break;
+			case 9:
+				System.out.println("Septiembre");
+				break;
+			case 10:
+				System.out.println("Octubre");
+				break;
+			case 11:
+				System.out.println("Noviembre");
+				break;
+			case 12:
+				System.out.println("Diciembre");
+				break;
+			default:
+				System.out.println("Ese Mes no existe");
+				break;
+		}
+		
 	}
 }
